f38541a10bc0459c8213418691a3aae4c146efee
TrevorDev <trevordev1978@protonmail.com>
2025-07-12 18:22:20 +0200

    Fix malformed tailwind.config.js (remove stray CSS)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
-
-
-theme: {
-  extend: {
-    fontFamily: {
-      signature: ['"Great Vibes"', 'cursive'],
+    extend: {
+      fontFamily: {
+        signature: ['"Great Vibes"', 'cursive'],
+      },
     },
   },
-}
-
-@import url('https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap');
-
-.font-signature {
-  font-family: 'Great Vibes', cursive;
-}
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
